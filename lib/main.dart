e1c315d2c95cec097714fec6d2f597c423c7e877
Sherrif <msherrif04@gmail.com>
2024-04-15 17:21:36 +0000

    initial commit and set up

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Clone',
+      theme: ThemeData.dark(),
+      home: const Scaffold(body: Text('Lets Build Flutter!')),
+    );
+  }
+}
